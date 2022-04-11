d792368dd9a397e30ba08d1f56059e358f083c90
Cesar Raymundo <56236031+CesarRaymundo@users.noreply.github.com>
2022-04-10 21:41:16 -0600

    Added more

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TextField from "@mui/material/TextField";
+var studentlist = [
+  { id: 1, name: 'Cesar Raymundo', present: false },
+  { id: 2, name: 'Andrew Gerber', present: false },
+  { id: 3, name: 'Zeynep Ozarslan', present: false },
+];
+
+const [ashowme, setshowme] = useState(false);
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <View>
+    <Button title="Add a Student" onPress={() => addButton()}/>
+    <table>
+      <tr>
+        <th className="studentName">Student Name</th>
+        <th className="attendance">Attendance</th>
+      </tr>
+      {studentlist.map((student) => (
+        <tr>
+          <td className="studentName">{student.name}</td>
+          <td className="attendance">
+            <input type="checkbox" />
+          </td>
+        </tr>
+      ))}
+    </table>
+    <Button title="submit" />
+
+
+  </View>
   );
+
 }
 
 function SettingsScreen() {
@@ -30,6 +56,9 @@ function MyTabs() {
   );
 }
 
+function addButton(){
+  
+}
 export default function App() {
   return (
     <NavigationContainer>
